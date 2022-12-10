1f36af825b2fb40131597ebe0b8d34bab1266b46
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2022-12-10 18:39:15 +0300

    ghghg

--- ChatAppAPI/ParticalClass/ChatRoomPart.cs
+++ ChatAppAPI/ParticalClass/ChatRoomPart.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace ChatAppAPI.Models
 {
@@ -8,7 +9,8 @@ namespace ChatAppAPI.Models
         {
             get
             {
-                string message = "Sweep";
+                string message = "Сообщение";
+                string Error = "ERROR 2041";
                 try
                 {
                     message = ChatMessageTable.LastOrDefault()?.TextMessage ?? "Нет сообщений";
@@ -16,7 +18,47 @@ namespace ChatAppAPI.Models
                 }
                 catch
                 {
-                    return message;
+                    return Error;
+                }
+            }
+        }
+
+        private ChatCollegeDataBaseEntities db = new ChatCollegeDataBaseEntities();
+        public string LastUser
+        {
+            get
+            {
+                int IdUser = 0;
+                string Null = "";
+                try
+                {
+                    IdUser = (int)(ChatMessageTable.LastOrDefault()?.PNUsers ?? null);
+                    var User = db.UsersTable.FirstOrDefault(data => data.PersonalNumberUser == IdUser);
+                    return User.NameUser.ToString() + ":";
+                }
+                catch
+                {
+                    return Null;
+                }
+            }
+        }
+
+        public string LastTime
+        {
+            get
+            {
+                DateTime datetime = new DateTime();
+                string Null = "";
+
+                try
+                {
+                    datetime = (DateTime)(ChatMessageTable.LastOrDefault()?.DataTime ?? null);
+                    DateTime time = datetime;
+                    return time.ToShortTimeString();
+                }
+                catch
+                {
+                    return Null;
                 }
             }
         }
--- ChatAppAPI/Respons/ResponceChatRoom.cs
+++ ChatAppAPI/Respons/ResponceChatRoom.cs
@@ -9,9 +9,15 @@ namespace ChatAppAPI.Respons
             PersonalNumberChatRoom = chatRoomTable.PersonalNumberChatRoom;
             TopicChatRoom = chatRoomTable.TopicChatRoom;
             GetLastMessage = chatRoomTable.LastMessage;
+            GetLastUser = chatRoomTable.LastUser;
+            GetLastTime = chatRoomTable.LastTime;
+           
         }
         public int PersonalNumberChatRoom { get; set; }
         public string TopicChatRoom { get; set; }
         public string GetLastMessage { get; set; }
+        public string GetLastUser { get; set; }
+        public string GetLastTime { get; set; }
+        
     }
 }
